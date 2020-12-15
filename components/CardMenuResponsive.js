9c962318c1e78f513faa52b5cd0164dfb2fa5f59
developerOmega <developerOmega@developerOmega.com>
2020-12-14 22:07:57 -0600

    Desarrollo de menu responsivo

--- components/CardMenuResponsive.js
+++ components/CardMenuResponsive.js
@@ -5,29 +5,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const CardMenuResponsive = ({resp}) => {
+  const [exit, setExit] = useState(false);
+
+  const classIndex = exit ?  styles.animationInRight : styles.animationInLeft;
+
+  const exitMenu = () => {
+    setExit(true);
+    setTimeout(() => resp(), 460);
+  }
 
   return (
-    <section className={ styles.index } >
+    <section className={styles.index + ' ' + classIndex} >
 
-      <button className={styles.iconDelete}> <FontAwesomeIcon icon={faTimes} onClick={ resp }  /> </button>
+      <button className={styles.iconDelete}> <FontAwesomeIcon icon={faTimes} onClick={ exitMenu }  /> </button>
 
       <h1 className={styles.titleIndex}> Comic Web </h1>
 
       <div className={styles.opt}>
         <Link href='/characters'>
-          <a className={styles.linkIndex}>Characters</a>
+          <a className={styles.linkIndex} onClick={ exitMenu } >Characters</a>
         </Link>
       </div>
 
       <div className={styles.opt}>
         <Link href='/comics'>
-          <a className={styles.linkIndex}> Comics </a>
+          <a className={styles.linkIndex} onClick={ exitMenu } > Comics </a>
         </Link>
       </div>
 
       <div className={styles.opt}>
         <Link href='/series'>
-          <a className={styles.linkIndex}> Series </a>
+          <a className={styles.linkIndex} onClick={ exitMenu } > Series </a>
         </Link>
       </div>
     </section>
