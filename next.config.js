9f407e07e1600a91e0dfecdcb8a82944f869c967
developerOmega <developerOmega@developerOmega.com>
2020-12-16 16:03:50 -0600

    Modificar imagenes

--- next.config.js
+++ next.config.js
@@ -34,9 +34,5 @@ module.exports = {
     domains: ['i.annihil.us'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
-  },
-  env: {
-    PUBLIC_KEY: 'cfd24420303411073f002d1d39e26031', 
-    PRIVATE_KEY: '302757dec4320cb2853505f9a0eb522e2f29f635'
   }
 }
